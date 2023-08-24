71dd4647ed2bf123e9ac40aaa7da808a549203a3
thanhgit123 <hai880345@gmail.com>
2023-08-24 21:06:43 +0700

    add

---
+++ luyentap1/baitap.js
@@ -0,0 +1,21 @@
+// baitap hinh tam giac
+// for(let i =1;i<=10;i++){
+//     for(let j=10-i;j>0;j--) {
+//         document.write("")
+//         document.write("&nbsp&nbsp&nbsp&nbsp")
+//     }
+//     for(let k=1;k<=i;k++){
+//         document.write("*")
+//         document.write("&nbsp&nbsp")
+
+//     }
+//     document.write("<br>");
+// }
+
+// bai 1:vong lap 1-->100
+//  for(let i=1; i <= 100;i++){
+//     console.log("i",i);
+//     if(i==99){
+//         alert("dda hoan thanhf");
+//      }
+//  }
